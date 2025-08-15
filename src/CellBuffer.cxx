22cbd4f912947d15f0066c8d02866a0aa217fcd2
Christo Joseph <christo.j@tataelxsi.co.in>
2025-08-15 17:09:17 +0530

    Revert "Backport: Encapsulate common check for PS, LS, and NEL as UTF8IsMultibyteLineEnd."
    This reverts commit 9b9c12efef67a4f327f298e36a75b19d97a4735a.

--- src/CellBuffer.cxx
+++ src/CellBuffer.cxx
@@ -734,7 +734,8 @@ bool CellBuffer::ContainsLineEnd(const char *s, Sci::Position length) const noex
 		if ((ch == '\r') || (ch == '\n')) {
 			return true;
 		} else if (utf8LineEnds) {
-			if (UTF8IsMultibyteLineEnd(chBeforePrev, chPrev, ch)) {
+			const unsigned char back3[3] = { chBeforePrev, chPrev, ch };
+			if (UTF8IsSeparator(back3) || UTF8IsNEL(back3 + 1)) {
 				return true;
 			}
 		}
@@ -906,7 +907,8 @@ void CellBuffer::ResetLineEnds() {
 				lineInsert++;
 			}
 		} else if (utf8LineEnds) {
-			if (UTF8IsMultibyteLineEnd(chBeforePrev, chPrev, ch)) {
+			const unsigned char back3[3] = {chBeforePrev, chPrev, ch};
+			if (UTF8IsSeparator(back3) || UTF8IsNEL(back3+1)) {
 				InsertLine(lineInsert, (position + i) + 1, atLineStart);
 				lineInsert++;
 			}
@@ -1079,7 +1081,8 @@ void CellBuffer::BasicInsertString(Sci::Position position, const char *s, Sci::P
 			InsertLine(lineInsert, (position + ptr - s), atLineStart);
 			lineInsert++;
 		} else if (utf8LineEnds && !UTF8IsAscii(ch)) {
-			if (UTF8IsMultibyteLineEnd(chBeforePrev, chPrev, ch)) {
+			const unsigned char back3[3] = { chBeforePrev, chPrev, ch };
+			if (UTF8IsSeparator(back3) || UTF8IsNEL(back3 + 1)) {
 				InsertLine(lineInsert, (position + ptr - s), atLineStart);
 				lineInsert++;
 			}
