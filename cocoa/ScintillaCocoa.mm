Matthew-McRaven/ScintillaQML

Bug [#2061]. Fix a crash that occurred when entering a dead key diacritic then a character that can not take that diacritic, such as option+e (acute accent) followed by g.

cocoa/ScintillaCocoa.mm
@@ -2179,7 +2179,18 @@ ptrdiff_t ScintillaCocoa::InsertText(NSString *input) {
 	std::string encoded = EncodedBytesString((__bridge CFStringRef)input, encoding);
 
 	if (encoded.length() > 0) {
-		AddCharUTF(encoded.c_str(), static_cast<unsigned int>(encoded.length()), false);
+		if (encoding == kCFStringEncodingUTF8) {
+			// There may be multiple characters in input so loop over them
+			std::string_view sv = encoded;
+			while (sv.length()) {
+				const unsigned char leadByte = sv[0];
+				const unsigned int bytesInCharacter = UTF8BytesOfLead[leadByte];
+				AddCharUTF(sv.data(), bytesInCharacter, false);
+				sv.remove_prefix(bytesInCharacter);
+			}
+		} else {
+			AddCharUTF(encoded.c_str(), static_cast<unsigned int>(encoded.length()), false);
+		}
 	}
 	return encoded.length();
 }

josephch/scintilla

Backport of changeset 7171:d07ee885f2dd, but translated to C++11.

cocoa/ScintillaCocoa.mm
@@ -2284,7 +2284,18 @@ ptrdiff_t ScintillaCocoa::InsertText(NSString *input) {
 
   if (encoded.length() > 0)
   {
-    AddCharUTF(encoded.c_str(), static_cast<unsigned int>(encoded.length()), false);
+    if (encoding == kCFStringEncodingUTF8) {
+      // There may be multiple characters in input so loop over them
+      std::string sv = encoded;
+      while (sv.length()) {
+        const unsigned char leadByte = sv[0];
+        const unsigned int bytesInCharacter = UTF8BytesOfLead[leadByte];
+        AddCharUTF(sv.c_str(), bytesInCharacter, false);
+        sv = sv.substr(bytesInCharacter, sv.length());
+      }
+    } else {
+      AddCharUTF(encoded.c_str(), static_cast<unsigned int>(encoded.length()), false);
+		}
   }
   return encoded.length();
 }